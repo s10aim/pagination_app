7e84b7766c6f03a7f8ea09ce08ad0b1e2c5227a1
tanahara <s10.vermouth.4869@gmail.com>
2021-05-13 16:12:05 +0900

    CRUD処理を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order(id: :desc)
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
